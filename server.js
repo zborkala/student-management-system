d58b685cc1588caf53045828bf2d7d8adaff6efb
Zubair Borkala <zborkala@gmail.com>
2024-07-09 16:07:02 +0530

    Add student setup completed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -10,19 +11,20 @@ const connection = mysql.createConnection({
     database: 'studentdb'
 });
 
-connection.connect((err) => {
-    if(err) {
-        console.log('Connection Error!', err);
-    } else {
-        console.log('Connected successfully!');
-    }
-});
+// connection.connect((err) => {
+//     if(err) {
+//         console.log('Connection Error!', err);
+//     } else {
+//         console.log('Connected successfully!');
+//     }
+// });
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 
     "views"));
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     connection.query(`SELECT * FROM students`, (err, result) => {
@@ -35,8 +37,28 @@ app.get('/', (req, res) => {
 });
 
 app.get('/add_student', (req, res) => {
-    
+    res.render('add_student');
 });
+
+app.post('/store_student', (req, res) => {
+    const body = req.body;
+    const name = body.name;
+    const age = body.age;
+    const phone = body.phone;
+    const gender = body.gender;
+    const className = body.class;
+
+    connection.query(`INSERT INTO students (name, age, gender, class, phone) VALUES ('${name}', ${age}, '${gender}', '${className}', '${phone}')`, (err, result) => {
+        if(err) {
+            res.send('Error Occured');
+            res.end();
+        } else {
+            res.redirect('/');
+        }
+    })
+
+
+})
 
 app.listen(4000, () => {
     console.log('Server is running...');
